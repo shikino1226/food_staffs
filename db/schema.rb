0f45eb10ba82efecb45064447d3b05f8f0c1ea4f
shikino <sshhiikkiinnoo@gmail.com>
2021-12-26 14:28:51 +0900

    食材登録画面をscaffoldで作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_045107) do
+ActiveRecord::Schema.define(version: 2021_12_26_052557) do
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "expiry_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
