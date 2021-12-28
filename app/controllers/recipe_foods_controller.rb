52dd5607f7cb3fca9b2e073f70c2d01471b60612
shikino <sshhiikkiinnoo@gmail.com>
2021-12-28 22:56:08 +0900

    食材追加・削除機能編集

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -22,18 +22,29 @@ class RecipeFoodsController < ApplicationController
   # POST /recipe_foods or /recipe_foods.json
   def create
     @recipe_food = RecipeFood.new(recipe_food_params)
-    if @recipe_food.update(recipe_food_params)
-      redirect_to (recipe_url (@recipe_food.recipe.id))
+    respond_to do |format|
+      if @recipe_food.save
+        format.html { redirect_to (recipe_url (@recipe_food.recipe.id)), notice: "Recipe food was successfully created." }
+        format.json { render :show, status: :created, location: @recipe_food }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /recipe_foods/1 or /recipe_foods/1.json
   def update
-    if @recipe_food.update(recipe_food_params)
-      redirect_to (recipe_url (@recipe_food.recipe.id))
+    respond_to do |format|
+      if @recipe_food.update(recipe_food_params)
+        format.html { redirect_to (recipe_url (@recipe_food.recipe.id)), notice: "Recipe food was successfully updated." }
+        format.json { render :show, status: :ok, location: @recipe_food }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
+      end
     end
   end
-
   # DELETE /recipe_foods/1 or /recipe_foods/1.json
   def destroy
     @recipe_food.destroy
