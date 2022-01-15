8baef8a177233da70b8c9c23f703127a3f4adeed
shikino <sshhiikkiinnoo@gmail.com>
2022-01-15 23:02:20 +0900

    rspec導入

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module FoodStaff
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
--- test/models/recipe_process_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class RecipeProcessTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
